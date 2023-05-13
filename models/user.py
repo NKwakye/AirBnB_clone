e0132bc33d28340444e750b155c0d1e8b0125168
NKwakye <56074884+NKwakye@users.noreply.github.com>
2023-05-13 21:31:24 +0000

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
-""" Class user """
+"""This is the User Model module.
 
+Contains the User class that inherits from BaseModel.
+"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """ Class user """
+    """This class defines a User.
+
+    Attributes:
+        email (str): the user's email.
+        password (str): the user's password.
+        first_name (str): the user's first_name.
+        last_name (str): the user's last_name.
+    """
+
     email = ""
     password = ""
     first_name = ""
